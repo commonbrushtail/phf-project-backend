1a651dde5f901675d33856b1762794cde1819190
BRUSHTAIL-PC\commo <commonbrushtail@gmail.com>
2023-12-13 01:00:59 +0700

    set up cors for frontend and auth/google in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+interface authDto {
+  credential: string;
+  clientId: string;
+}
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('google')
+  async recieveToken(@Body() authObject: authDto) {
+    console.log(authObject);
+  }
+}
