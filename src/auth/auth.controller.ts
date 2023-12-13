4f294b630fd77a2383e99dde3d257b0d4b9586fa
BRUSHTAIL-PC\commo <commonbrushtail@gmail.com>
2023-12-14 00:48:03 +0700

    auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
-
+import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
   @Post('google')
   async recieveToken(@Body() authObject: AuthDto) {
-    console.log(authObject);
+    try {
+      const payload = await this.authService.verifyGoogleAuthToken(
+        authObject.credential,
+      );
+      console.log(payload);
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user.entity';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private oauthClient: OAuth2Client;
+
+  constructor(
+    private configService: ConfigService,
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {
+    this.oauthClient = new OAuth2Client(configService.get('GOOGLE_CLIENT_ID'));
+  }
+
+  async verifyGoogleAuthToken(idToken: string) {
+    try {
+      const ticket = await this.oauthClient.verifyIdToken({
+        idToken,
+        audience: this.configService.get('GOOGLE_CLIENT_ID'),
+      });
+
+      return ticket.getPayload();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
